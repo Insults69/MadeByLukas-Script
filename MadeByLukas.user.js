bfa605b10595bcb71eb5a66e5c76e42ad8c72542
Insults69 <Insults3@gmail.com>
2026-02-02 20:31:16 +0200

    Update MadeByLukas.user.js

--- MadeByLukas.user.js
+++ MadeByLukas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MadeByLukas
 // @namespace    madebylukas
-// @version      1.0.0
+// @version      1.0.1
 // @description  Loader + Update UI
 // @match       https://*.tankionline.com/play/
 // @match       https://*.tankionline.com/browser-public/*
